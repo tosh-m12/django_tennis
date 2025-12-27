ca31aa0bcd4907730ff69f7e2bdabae165f4e32c
Murayama Tosh (村山 敏夫) <toshio.murayama@nipponexpress.com.cn>
2025-12-27 19:24:41 +0800

    Add release command to create superuser

---
+++ tennis/management/commands/create_admin.py
@@ -0,0 +1,15 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        User = get_user_model()
+        if not User.objects.filter(username="admin").exists():
+            User.objects.create_superuser(
+                username="admin",
+                email="admin@example.com",
+                password="StrongPassword123!"
+            )
+            self.stdout.write("superuser created")
+        else:
+            self.stdout.write("superuser already exists")
